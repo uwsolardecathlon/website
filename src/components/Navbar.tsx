967954829f4318984c3c0d6391ecee07cb842d78
Cheng <chengh24@uw.edu>
2024-02-16 17:48:30 -0800

    got a functional menu going

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,24 @@
+import {useState} from 'react';
 import { NavLink } from 'react-router-dom';
 import { RiMenuLine as Open } from 'react-icons/ri';
+import { useTransition, animated } from '@react-spring/web';
+import { Fade as Hamburger } from 'hamburger-react';
 
 import Button from './Button.tsx';
-import NavItem from './NavItem.tsx';
+import NavItem from './Navitem.tsx';
 
 function Navbar() {
+  const before = (window.innerWidth * -100) / 100
+  const [show, setShow] = useState(false)
+  const transition = useTransition(show,
+  {
+    from: {x: before, opacity: 0},
+    enter: {x: 0, opacity: 1},
+    leave: {x: before, opacity: 0}
+  });
+  
   return (
-    <nav>
+    <nav className='fixed w-full z-50'>
       {/* Desktop */}
       <div className='hidden md:flex flex-row justify-between gap-17 bg-opacity-0 mx-0 px-11 py-5 items-center'>
         <p>Logo</p>
@@ -24,20 +36,21 @@ function Navbar() {
       </div>
 
       {/* Mobile */}
-      <div className='flex justify-between items-center p-4 md:hidden'>
-        <p>Logo</p>
-        <Open size={32} />
-        {/* <ul className={'flex flex-row justify-between gap-8'}>
-          <NavItem label='home' />
-          <NavItem label='team' />
-          <NavItem label='competition' />
-          <NavItem label='donate' />
-          <li>
-            <NavLink to='/join' tabIndex={-1}>
-              <Button label='join' className='uppercase' />
-            </NavLink>
-          </li>
-        </ul> */}
+      <div className='flex flex-col items-center md:hidden'>
+        <div className='flex w-full h-[10vh] justify-between items-center p-4'>
+          <p>Logo</p>
+          <Hamburger toggle={setShow} toggled={show} size={25} />
+        </div> 
+        {transition((style, item) =>
+         item ? 
+          <animated.ul style={style} className={'item flex relative opacity-10 text-xl w-full h-[90vh] justify-items-center justify-around flex-wrap flex-col bg-neutral-400/50'}>  
+            <NavItem label='home' />
+            <NavItem label='team' />
+            <NavItem label='competition' />
+            <NavItem label='donate' />
+            <NavItem label='join' />
+          </animated.ul> : ''
+        )}
       </div>
     </nav>
   );
