f9c23baed4a7e1b1ba950b154cc5d3b3d685664d
Cheng <chengh24@uw.edu>
2024-03-20 18:50:19 -0700

    competition done, still need more content

--- src/components/Timelineitem.tsx
+++ src/components/Timelineitem.tsx
@@ -1,3 +1,4 @@
+import Subtitle1 from "./text/Subtitle1";
 
 interface TLProps {
     label: string;
@@ -9,8 +10,13 @@ function TLItem(TLProp: TLProps) {
     const {label = 'N/A', content = 'N/A', number = 'N/A'} = TLProp;
     
     return (
-        <>
-        </>
+        <li className="flex flex-col gap-3 divide-y divide-neutral-400 max-w-64">
+            <p className="flex flex-row gap-4">
+                <div className="text-neutral-300">0{number}.</div> 
+                Phase {number}: {label}
+            </p>
+            <p className="pt-3">{content}</p>
+        </li>
     );
 }
 
--- src/pages/Competition.tsx
+++ src/pages/Competition.tsx
@@ -1,9 +1,8 @@
-import Button from "../components/Button.tsx";
-import { GoArrowRight } from "react-icons/go";
 import H1 from "../components/text/H1.tsx";
 import H2 from "../components/text/H2.tsx";
 import Subtitle1 from "../components/text/Subtitle1.tsx";
 import Subtitle2 from "../components/text/Subtitle2.tsx";
+import TLItem from "../components/Timelineitem.tsx";
 const Competition = () => {
   return (
     <>
@@ -47,8 +46,15 @@ const Competition = () => {
           </p>
         </div>
       </section>
-      <section className="flex flex-row mt-28 items-center">
-
+      <section className="flex flex-col gap-7 mt-28">
+        <Subtitle2>Something</Subtitle2>
+        <H2>Project Timeline</H2>
+        <ul className="flex flex-row justify-between">
+          <TLItem label="Project Summary" number="1" content="The first competition stage is the Project Summary, which consists of an optional deliverable. The Project Summary provides an opportunity to submit preliminary information about a team’s project. Though optional, it is highly recommended that teams submit the Project Summary to gain preliminary, qualitative feedback to improve and iterate upon their design. It also familiarizes teams with the submission process and how to comply with Rules requirements."/>
+          <TLItem label="Semifinals" number="2" content="The second competition stage is the Semifinal Competition, which consists of a deliverable and presentation to Jurors. The Semifinal Competition Event is a virtual event during which all Participant Teams present to panels of industry expert Jurors to compete for selection as Finalist Teams that advance to the final stage of the competition. All teams who intend to continue participation in the competition must present virtually during their assigned time at the Semifinal Competition Event. "/>
+          <TLItem label="Finals" number="3" content="The third competition stage is the Final Submission, which consists of a deliverable. The Final Submission deliverable fully documents the final design. The Solar Decathlon Competition Event, to be held in April, is the culmination of the competition and consists of a presentation to Jurors. The Competition Event provides a rich experience for participants to present their final design projects to industry experts, engage in networking opportunities, listen to thought leader speakers, and connect with both peers and leading experts in the building industry. Finalist Teams compete for Division and Grand Winner Awards, and Exhibition Teams present to reviewers for feedback."/>
+          <TLItem label="Title" number="4" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quod, voluptate, quia, voluptates quas voluptatibus quibusdam quae quidem quos quas voluptatibus quibusdam quae."/>
+        </ul>
       </section>
     </>
   );
