aeccd36ddf3765f695177045a66edfd277683187
ebarron1 <ebarron1@uw.edu>
2024-03-27 16:59:12 -0700

    worked on donate and join page

--- src/pages/Donate.tsx
+++ src/pages/Donate.tsx
@@ -1,5 +1,34 @@
+import Button from "../components/Button.tsx";
+import H1 from "../components/text/H1";
+import H2 from "../components/text/H2";
+
 const Donate = () => {
-  return <div>Donate</div>;
+  return(
+    <>
+      <section className="flex flex-col gap-20 mt-40">
+        <H1>Support</H1>
+      </section>
+
+      <br />
+
+      <section className="flex flex-row mt-28 gap-20 items-center">
+        <div className="flex flex-col gap-7">
+          <H2>Donate</H2>
+          <p>
+            We are extremely grateful to those who have supported our
+            journey so far. We are looking to expand our focus and
+            message to students and industry to provide a meaningful
+            impact. If you are willing to support us on our journey, please
+            click on the donation button below.
+          </p>
+          <div>
+            <Button label="Donate" />
+          </div>
+        </div>
+        <img className="h-auto max-w-[555px]" src="/images/about_origin.png" alt="Image of Gould Hall (change later)"/>
+      </section>
+    </>
+  );
 };
 
 export default Donate;
--- src/pages/Join.tsx
+++ src/pages/Join.tsx
@@ -1,5 +1,33 @@
+import Button from "../components/Button.tsx";
+import H1 from "../components/text/H1";
+import H2 from "../components/text/H2";
+
 const Join = () => {
-  return <div>Join</div>;
+  return(
+    <>
+      <section className="flex flex-col gap-20 mt-40">
+        <H1>Join</H1>
+      </section>
+
+      <section className="flex flex-row mt-28 gap-20 items-center">
+        <div className="flex flex-col gap-7 w-2/5">
+          <H2>How do I join?</H2>
+          <p>
+            The University of Solar Decathlon is always looking for
+            new members to join our team. Through interdisciplinary
+            project management, we are looking to get all students
+            into the clean energy workforce. If you are interested
+            in participating, please click the Apply button below.
+          </p>
+          <div>
+            <Button label="Apply"/>
+          </div>
+        </div>
+      </section>
+
+      <section></section>
+    </>
+  );
 };
 
 export default Join;
