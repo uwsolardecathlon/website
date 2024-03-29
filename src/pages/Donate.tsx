c117e78d64e080e9b96b02f753f711a4935ba2f9
annawng <annawang125@gmail.com>
2024-03-29 16:13:48 -0700

    Fix x padding on Join and Donate pages

--- src/pages/Donate.tsx
+++ src/pages/Donate.tsx
@@ -1,31 +1,35 @@
-import Button from "../components/Button.tsx";
-import H1 from "../components/text/H1";
-import H2 from "../components/text/H2";
+import Button from '../components/Button.tsx';
+import H1 from '../components/text/H1';
+import H2 from '../components/text/H2';
 
 const Donate = () => {
-  return(
+  return (
     <>
-      <section className="flex flex-col gap-20 mt-40">
+      <section className='flex flex-col gap-20 mt-40 container'>
         <H1>Support</H1>
       </section>
 
       <br />
 
-      <section className="flex flex-row mt-28 gap-20 items-center">
-        <div className="flex flex-col gap-7">
+      <section className='flex flex-row my-28 gap-20 items-center container'>
+        <div className='flex flex-col gap-7'>
           <H2>Donate</H2>
           <p>
-            We are extremely grateful to those who have supported our
-            journey so far. We are looking to expand our focus and
-            message to students and industry to provide a meaningful
-            impact. If you are willing to support us on our journey, please
-            click on the donation button below.
+            We are extremely grateful to those who have supported our journey so
+            far. We are looking to expand our focus and message to students and
+            industry to provide a meaningful impact. If you are willing to
+            support us on our journey, please click on the donation button
+            below.
           </p>
           <div>
-            <Button label="Donate" />
+            <Button label='Donate' />
           </div>
         </div>
-        <img className="h-auto max-w-[555px]" src="/images/about_origin.png" alt="Image of Gould Hall (change later)"/>
+        <img
+          className='h-auto max-w-[555px]'
+          src='/images/about_origin.png'
+          alt='Image of Gould Hall (change later)'
+        />
       </section>
     </>
   );
