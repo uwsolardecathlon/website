377cc03e8bac40ce7e862843213b892c90ccee02
annawng <annawang125@gmail.com>
2024-05-04 15:47:12 -0700

    Fix About page layout

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -15,7 +15,7 @@ const PageHeader = ({
   imageAlt,
 }: PageHeaderProps) => {
   return (
-    <section className='container flex flex-col max-sm:text-center gap-4 sm:gap-12 pb-12 sm:pb-20'>
+    <section className='container flex flex-col max-sm:text-center gap-4 sm:gap-8 md:gap-12 pb-12 md:pb-20'>
       <div className='flex flex-col gap-4'>
         <H1>{title}</H1>
         <Subtitle1 className='leading-tight max-sm:mx-auto max-w-56 sm:max-w-[400px]'>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,16 +3,16 @@ import PageHeader from '../components/PageHeader.tsx';
 
 const About = () => {
   return (
-    <div className='my-28'>
+    <div className='mt-24 mb-16 md:mt-32 md:mb-20'>
       <PageHeader
         title='About'
         subtitle='Learn about the history of our team and what we do.'
         imageSrc='/images/about_hero.png'
       />
-      <section className='lg:flex lg:mt-0 mt-10 block items-center container'>
-        <div className='lg:w-1/2 lg:pr-20 flex flex-col gap-4'>
+      <section className='flex max-md:flex-col max-md:gap-8 items-start lg:items-center container'>
+        <div className='md:w-1/2 md:pr-12 lg:pr-20 flex flex-col gap-4'>
           <H2>Team History</H2>
-          <div className='flex flex-col gap-5'>
+          <div className='flex flex-col gap-2'>
             <p>
               Founded by a student in Autumn 2023, the University of Washington
               Solar Decathlon team is the first Building Science RSO on campus.
@@ -20,41 +20,42 @@ const About = () => {
               passion for renewable energy.
             </p>
             <p>
-              first competed in the 2024 Solar Decathlon as a team of 20 students.
-              With the support of our faculty lead, Rob Pena, we look forward to
-              future competitions and the continued growth of our club!
+              We first competed in the 2024 Solar Decathlon as a team of 20
+              students. With the support of our faculty lead, Rob Pena, we look
+              forward to future competitions and the continued growth of our
+              club!
             </p>
           </div>
         </div>
         <img
-          className='lg:w-1/2 w-full mt-10 h-auto align'
+          className='w-full md:w-1/2'
           src='/images/about_origin.png'
-          alt='image of Gould Hall'
+          alt=''
         />
       </section>
-      <section className='lg:flex block mt-10 items-center container'>
-        <div className='lg:w-1/2 lg:pl-20 flex flex-col gap-4'>
+      <section className='flex max-md:flex-col max-md:gap-8 mt-12 md:mt-20 items-start lg:items-center container'>
+        <div className='md:w-1/2 md:pl-12 lg:pl-20 flex flex-col gap-4'>
           <H2>Reaching Net Zero</H2>
-          <div className='flex flex-col gap-5'>
+          <div className='flex flex-col gap-2'>
             <p>
-              The Paris Agreement was a document signed by over 190 countries in 
-              2015 to reduce carbon emissions by 45% by 2030 and achieve net zero 
-              by 2050. Since buildings account for approximately 35% of carbon
-              emissions in the US and 40% of its energy usage, the Solar Decathlon 
-              challenges students to design low-carbon, net zero energy homes for 
-              disadvantaged communities.
+              The Paris Agreement was a document signed by over 190 countries in
+              2015 to reduce carbon emissions by 45% by 2030 and achieve net
+              zero by 2050. Since buildings account for approximately 35% of
+              carbon emissions in the US and 40% of its energy usage, the Solar
+              Decathlon challenges students to design low-carbon, net zero
+              energy homes for disadvantaged communities.
             </p>
             <p>
-              For students preparing to join the clean energy workforce, the Solar
-              Decathlon is an excellent opportunity to gain real-life experience
-              designing buildings for a more sustainable world.
+              For students preparing to join the clean energy workforce, the
+              Solar Decathlon is an excellent opportunity to gain real-life
+              experience designing buildings for a more sustainable world.
             </p>
           </div>
         </div>
         <img
-          className='lg:order-first lg:w-1/2 w-full h-auto mt-10'
+          className='w-full md:w-1/2 md:order-first'
           src='/images/about_origin.png'
-          alt='image of Gould Hall'
+          alt=''
         />
       </section>
     </div>
