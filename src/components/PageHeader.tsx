882a92a2cd9779ca2b54f1b856fc7913f1ec49e7
annawng <annawang125@gmail.com>
2024-05-03 17:43:31 -0700

    Fix PageHeader component

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -15,26 +15,19 @@ const PageHeader = ({
   imageAlt,
 }: PageHeaderProps) => {
   return (
-    <>
-      {/* Desktop */}
-      <section className='lg:flex hidden flex-col gap-12 container pb-20'>
-        <div>
-          <H1>{title}</H1>
-          <Subtitle1 className='mt-4 max-w-md'>{subtitle}</Subtitle1>
-        </div>
-        <img className='h-auto max-w-full' src={imageSrc} alt={imageAlt ?? ''} />
-      </section>
-
-      {/* Mobile */}
-      <section className='lg:hidden flex flex-col md:gap-20 gap-16 container'>
-        <div className='text-center'>
-          <H1>{title}</H1>
-          <Subtitle1 className='mt-4 max-w-[670px] mx-auto'>{subtitle}</Subtitle1>
-        </div>
-        <img className='h-auto max-w-full' src={imageSrc} alt={imageAlt ?? ''} />
-      </section>
-    </>
-
+    <section className='container flex flex-col max-sm:text-center gap-4 sm:gap-12 pb-12 sm:pb-20'>
+      <div className='flex flex-col gap-4'>
+        <H1>{title}</H1>
+        <Subtitle1 className='leading-tight max-sm:mx-auto max-w-56 sm:max-w-[400px]'>
+          {subtitle}
+        </Subtitle1>
+      </div>
+      <img
+        className='h-auto aspect-[4/3] sm:aspect-[16/9] object-cover max-w-full'
+        src={imageSrc}
+        alt={imageAlt ?? ''}
+      />
+    </section>
   );
 };
 
