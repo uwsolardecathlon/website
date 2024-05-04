e7d5ebd064a114ae38bc5161f2d854c2f81e4c36
annawng <annawang125@gmail.com>
2024-05-03 17:22:00 -0700

    Move FAQ text to separate file

--- src/pages/Join.tsx
+++ src/pages/Join.tsx
@@ -2,34 +2,7 @@ import Button from '../components/Button.tsx';
 import H2 from '../components/text/H2';
 import PageHeader from '../components/PageHeader.tsx';
 import Accordion from '../components/Accordion.tsx';
-
-const faq = [
-  {
-    title: 'Do I need any experience to join the UW Solar Decathlon?',
-    content:
-      "No! As long as you're willing to learn, we will help you develop the skills required to contribute to our projects.",
-  },
-  {
-    title: 'What is the time commitment?',
-    content:
-      'The estimated time commitment varies by team. Visit the Team page for more details.',
-  },
-  {
-    title: 'What will I gain by joining the club?',
-    content:
-      'We actively train members in technical skills such as designing, energy modeling, embodied carbon modeling, and financial modeling. By providing this support, we hope to give all members hands-on experience with tackling problems that affect the world. We also actively interact with industry professionals, which can provide opportunities for internships, scholarships and more.',
-  },
-  {
-    title: 'What kind of work can I do for the club?',
-    content:
-      'In addition to our competition team, we are looking for members to expand the club through education, outreach, finance and more. If you let us know your goals, we will do our best to accommodate you.',
-  },
-  {
-    title: 'How can I learn about upcoming opportunities?',
-    content:
-      "Once you apply to the club, you will be added to our club's Slack where we send announcements about events, opportunities for research, and more. Join if you're at all interested!",
-  },
-];
+import { faq } from '../data/faq.tsx';
 
 const Join = () => {
   return (
