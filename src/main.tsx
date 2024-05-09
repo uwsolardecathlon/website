32129bec8bb9e23dad97d8603035478c44a2c818
ebarron1 <ebarron1@uw.edu>
2024-05-08 17:02:57 -0700

    root to Root

--- src/main.tsx
+++ src/main.tsx
@@ -54,7 +54,7 @@ const router = createBrowserRouter([
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('Root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
