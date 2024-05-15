5fa549a667884bbc3c80f2cb8c51839d945f9840
Cheng <chengh24@uw.edu>
2024-05-15 16:52:35 -0700

    another small change

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -17,7 +17,7 @@ const Form = ({
   } = useForm({ mode: 'all' });
   const sendEmail = () => {
     const hiddenInput = (document.getElementById('hidden-input') as HTMLInputElement);
-    if (hiddenInput !== null && hiddenInput.value !== "") {
+    if (hiddenInput === null || hiddenInput.value !== "") {
       return;
     }
     if (form && form.current) {
